5b83291250bdb68cd258f713328144801f3f4b31
CosmicLM <edsldev25@gmail.com>
2026-01-11 19:23:34 -0700

    docs: comment logic to person.rs

---
+++ backend/src/domain/person.rs
@@ -0,0 +1,5 @@
+// step 1, identify the ID of the person
+// step 2, sort the name into distinct boxes, Given name, Surname, Prefix and SUffix
+// step 3, identify aproximate dates, and display raw text instead of the factual number
+// step 4, implement boxes for male, female and unknoen
+// step 5, connections to family, parents, spouses and children
