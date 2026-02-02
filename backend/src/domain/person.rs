7e2cb7d32eab0830e6221d27960e75e942c0c927
CosmicLM <edsldev25@gmail.com>
2026-02-02 10:18:45 -0700

    doc: add additional logical steps

--- backend/src/domain/person.rs
+++ backend/src/domain/person.rs
@@ -1,4 +1,6 @@
 // step 1, identify the ID of the person
+    //step 1.1, what would be ID on .ged file?
+    //step 1.2, what library would i need to detect this
 // step 2, sort the name into distinct boxes, Given name, Surname, Prefix and SUffix
 // step 3, identify aproximate dates, and display raw text instead of the factual number
 // step 4, implement boxes for male, female and unknoen
