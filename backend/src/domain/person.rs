17d9d013c4d9ae77986570b13730b8dbf9e14822
CosmicLM <edsldev25@gmail.com>
2026-03-01 16:05:18 -0700

    refactor: Define new person class

--- backend/src/domain/person.rs
+++ backend/src/domain/person.rs
@@ -1,4 +1,10 @@
 // step 1, identify the ID of the person
+pub struct PersonStats {
+    pub given_name: String,
+    pub surname: String,
+    pub gender: String,
+    pub id: String,
+}
     //step 1.1, what would be ID on .ged file?
     //step 1.2, gedcom.rs parses the id
 // step 2, sort the name into distinct boxes, Given name, Surname, Prefix and SUffix
