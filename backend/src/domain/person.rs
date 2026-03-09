4715e5794df92d3e7599c384f6378630632b56cb
CosmicLM <edsldev25@gmail.com>
2026-03-08 22:02:48 -0600

    refactor: fix person.rs functions and name

--- backend/src/domain/person.rs
+++ backend/src/domain/person.rs
@@ -1,12 +1,42 @@
 // step 1, identify the ID of the person
+    //step 1.2, gedcom.rs parses the id
+  
 pub struct PersonStats {
     pub given_name: String,
     pub surname: String,
-    pub gender: String,
+    pub enum gender {
+        Male,
+        Female,
+        Unknown,
+    } 
     pub id: String,
+
+ let result = gedcom::parse_file();
+
+ match result{
+    Ok(data) => {
+        for individual in data.individuals():
+        let current_id = individual.xref.to_string();
+
+        let Some(name) = individual.name().next() else { continue };
+        let surname = name.next() else { continue };
+
+
+ }
+
 }
-    //step 1.1, what would be ID on .ged file?
-    //step 1.2, gedcom.rs parses the id
+ Err(_) => {
+    println!("No parse file")
+ }
+
+     }
+     
+     
+     //locate string with 0
+    //idenfity the @ symbols
+    //confirmation of INDI (individual)
+
+
 // step 2, sort the name into distinct boxes, Given name, Surname, Prefix and SUffix
 // step 3, identify aproximate dates, and display raw text instead of the factual number
 // step 4, implement boxes for male, female and unknoen
